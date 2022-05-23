cc45a324bfc47eabdd7bb8fec5e88a5647c71bd7
ck <goceefake@gmail.com>
2022-05-23 23:36:01 +0200

    initial sql data added

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into customer (name, surname) values ('nazim', 'hikmet');
+insert into customer (name, surname) values ('salvador', 'dali');
+insert into customer (name, surname) values ('franz', 'kafka');
+insert into customer (name, surname) values ('bertrand', 'russell');
